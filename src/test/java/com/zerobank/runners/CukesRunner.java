28ba9f2e6fd3b353a188652475c4495477c78607
DavidAydin <tarikaydin@gmail.com>
2021-08-14 18:56:41 +0200

    Configured for all tests

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/zerobank/step_definitions",
-        dryRun = false,
-        tags = "@smoke"
+        dryRun = false//,
+      //  tags = ""
         
 )
 
